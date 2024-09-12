811aa519b9830881ac37a9605334912188eacadc
tiagofneto <tiagofneto29@gmail.com>
2024-09-12 12:26:10 +0100

    cors

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,10 +1,12 @@
 import { readFileSync } from 'fs';
 import { deploy, createCompany } from './aztec.mjs';
 import express from 'express';
+import cors from 'cors';
 
 const port = 3000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 async function initializeServer() {
     console.log("Initializing server");
