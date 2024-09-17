4ec38920e3c658f526be5091c5172d11c2c41b4e
tiagofneto <tiagofneto29@gmail.com>
2024-09-17 14:36:48 +0100

    drizzle orm and companies table

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.SUPABASE_DATABASE_URI as string
+  }
+});
---
+++ backend/src/schema.ts
@@ -0,0 +1,17 @@
+import {
+    pgTable,
+    serial,
+    varchar,
+    timestamp,
+    integer
+  } from 'drizzle-orm/pg-core';
+  
+  export const companies = pgTable('companies', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull(),
+    director: varchar('director', { length: 255 }).notNull(),
+    totalShares: integer('total_shares').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow()
+  });
+  
