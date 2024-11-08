f8c922045de962371772ebdbf0d4b65597734ad7
tiagofneto <tiagofneto29@gmail.com>
2024-11-08 14:56:27 +0000

    react-camera

--- frontend/app/user/kyc-dialog.tsx
+++ frontend/app/user/kyc-dialog.tsx
@@ -1,8 +1,19 @@
 import { BaseStepDialog, Step } from '@/components/base-step-dialog';
+import { Button } from '@/components/ui/button';
 import { OpenPassportQRcode } from '@openpassport/sdk';
-import { ReactNode } from 'react';
+import { ReactNode, useCallback, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import Image from 'next/image';
 
 export function KYCDialog({ children, userId }: { children: ReactNode, userId: string }) {
+    const webcamRef = useRef<Webcam>(null);
+    const [imgSrc, setImgSrc] = useState('');
+
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current?.getScreenshot();
+        if (imageSrc) setImgSrc(imageSrc);
+    }, [webcamRef]);
+
     const steps: Step[] = [
         {
             title: 'Passport',
@@ -17,6 +28,33 @@ export function KYCDialog({ children, userId }: { children: ReactNode, userId: s
                     devMode={true}
                     size={300}
                 />
+            )
+        },
+        {
+            title: 'Face',
+            description: 'Take a selfie to verify your identity.',
+            component: () => (
+                <>
+                    {imgSrc ? (
+                        <>
+                            <Image src={imgSrc} alt="Captured selfie" width={1280} height={720} />
+                            <Button onClick={() => setImgSrc('')}>Retake photo</Button>
+                        </>
+                    ) : (
+                        <>
+                            <Webcam
+                                audio={false}
+                                ref={webcamRef}
+                                screenshotFormat="image/jpeg"
+                                height={720}
+                                width={1280}
+                            />
+                            <Button onClick={capture}>
+                                Capture photo
+                            </Button>
+                        </>
+                    )}
+                </>
             )
         }
     ]
